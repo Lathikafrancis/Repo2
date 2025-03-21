ac6fb865d6751000d809e8bec377c1cbc6c2d8e4
LENOVO <LENOVO@DESKTOP-3P7B85N>
2025-03-21 12:07:08 +0530

    second commit

---
+++ Javaproject1/src/pack1/Name.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			 Name obj = new Name();
+			 obj.print();
+			}
+			public void print()
+			{
+			System.out.println("Lathika");
+			}
+}
